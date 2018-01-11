ce282006c10eeac174cda480da9e1624798ddfed
Gregory Norwood, Jr <gmajorincome@gmail.com>
2018-01-11 15:38:43 -0700

    jobcontroller complete

--- MVCProject/src/controllers/JobsController.java
+++ MVCProject/src/controllers/JobsController.java
@@ -21,14 +21,14 @@ public class JobsController {
 	@Autowired
 	private JobsDAO jobsdao;
 	
-//  GET /user/{id}/jobs
-	@RequestMapping(path = "/user/{id}/jobs", method = RequestMethod.GET)
+//  GET /user/{uid}/jobs
+	@RequestMapping(path = "/user/{uid}/jobs", method = RequestMethod.GET)
 	public List<Job> getAllJobs(
 			HttpServletRequest req, 
 			HttpServletResponse res, 
-			@PathVariable int id
+			@PathVariable int uid
 			){
-		return jobsdao.getAllJobs(id);
+		return jobsdao.getAllJobs(uid);
 	}
 	
 //  GET /user/{uid}/jobs/{jid}
@@ -36,9 +36,10 @@ public class JobsController {
 	public Job getJobById(
 			HttpServletRequest req, 
 			HttpServletResponse res, 
-			@PathVariable int id
+			@PathVariable int uid,
+			@PathVariable int jid
 			) {
-		Job j = jobsdao.getJobById(id);
+		Job j = jobsdao.getJobById(uid, jid);
 		
 		if(j == null) {
 			res.setStatus(404);
@@ -47,14 +48,50 @@ public class JobsController {
 	}
 	
 //  POST /user/{uid}/jobs
-	@RequestMapping(path="/user/{uid}/jobs", method=RequestMethod.PUT)
+	@RequestMapping(path="/user/{uid}/jobs", method=RequestMethod.POST)
 	public Job addNewJob(
 			HttpServletRequest req, 
 			HttpServletResponse res, 
 			@PathVariable int uid, 
 			@RequestBody String json) {
 		
-		Job job = jobsdao.addNewJob()
+		Job job = jobsdao.addNewJob(uid, json);
+		if(job == null) {
+			res.setStatus(400);
+		}
+		return job;
 	}
+	
+	
+//  PUT /user/{uid}/todo/{tid}
+	@RequestMapping(path="user/{uid}/jobs/{jid}", method=RequestMethod.PUT)
+	public Job updateJob(
+
+			HttpServletRequest req, 
+			HttpServletResponse res, 
+			@PathVariable int uid, 
+			@PathVariable int jid, 
+			@RequestBody String json) {
+		Job job = jobsdao.updateJob(uid, jid, json);
+		if(job ==null) {
+			res.setStatus(400);
+		}
+		return job;
+	}
+	
+//  DELETE /user/{uid}/jobs/{jid}
+	@RequestMapping(path="/user/{uid}/jobs/{jid}", method=RequestMethod.DELETE)
+	public Boolean destroyJob(
+			HttpServletRequest req, 
+			HttpServletResponse res, 
+			@PathVariable int uid,
+			@PathVariable int jid) {
+		Boolean result = jobsdao.destroyJob(uid, jid);
+		if(result == null) {
+			res.setStatus(404);
+		}
+		return result;
+	}
+			
 
 }
