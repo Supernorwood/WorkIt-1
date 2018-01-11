fd54f4b72b46a8c751c5f2992776060e700d5d8a
Gregory Norwood, Jr <gmajorincome@gmail.com>
2018-01-11 10:10:02 -0700

    entities package and classes created

---
+++ JPAProject/src/entities/Job.java
@@ -0,0 +1,25 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Job {
+	
+	  @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private int id;
+	  
+	  private String company;
+	  
+	  private String title;
+	  
+	  private String link;
+	  
+	
+	  
+	  
+
+}
