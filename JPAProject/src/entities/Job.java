eea0e74bf0b51466ce48975caffa101329627eb3
Gregory Norwood, Jr <gmajorincome@gmail.com>
2018-01-11 12:00:01 -0700

    job entity class complete with gets and sets

--- JPAProject/src/entities/Job.java
+++ JPAProject/src/entities/Job.java
@@ -1,16 +1,34 @@
 package entities;
 
+import java.sql.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 public class Job {
 	
+	
+	  //FIELDS
 	  @Id
 	    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	    private int id;
+	  
+	  
+	  @JsonBackReference
+	  @ManyToOne
+	  @JoinColumn(name = "user_id")
+	  private User user;
+	  
+	  private Boolean active;
 	  
 	  private String company;
 	  
@@ -18,6 +36,146 @@ public class Job {
 	  
 	  private String link;
 	  
+	  @OneToOne
+	  @JoinColumn(name="address_id")
+	  private Address address;
+	  
+	  private double salary;
+	  
+	  @Column(name = "contact_phone")
+	  private int contactPhone;
+	  
+	  @Column(name = "contact_email")
+	  private String contactEmail;
+	  
+	  @Column(name = "closing_date")
+	  private Date closingDate;
+	  
+	  @Column(name = "created_date")
+	  private Date createdDate;
+	  
+	  @Column(name = "last_update")
+	  private Date lastUpdate;
+
+	  
+	  
+	  
+	  
+	//GETS AND SETS  
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public int getContactPhone() {
+		return contactPhone;
+	}
+
+	public void setContactPhone(int contactPhone) {
+		this.contactPhone = contactPhone;
+	}
+
+	public String getContactEmail() {
+		return contactEmail;
+	}
+
+	public void setContactEmail(String contactEmail) {
+		this.contactEmail = contactEmail;
+	}
+
+	public Date getClosingDate() {
+		return closingDate;
+	}
+
+	public void setClosingDate(Date closingDate) {
+		this.closingDate = closingDate;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getLastUpdate() {
+		return lastUpdate;
+	}
+
+	public void setLastUpdate(Date lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+
+	@Override
+	public String toString() {
+		return "Job [id=" + id + ", user=" + user + ", active=" + active + ", company=" + company + ", title=" + title
+				+ ", link=" + link + ", address=" + address + ", salary=" + salary + ", contactPhone=" + contactPhone
+				+ ", contactEmail=" + contactEmail + ", closingDate=" + closingDate + ", createdDate=" + createdDate
+				+ ", lastUpdate=" + lastUpdate + "]";
+	}
+	
+	  
+	
 	
 	  
 	  
