5e993d83ba8588abf0939e6d7a8836e9862b75f7
Gregory Norwood, Jr <gmajorincome@gmail.com>
2018-01-11 10:19:08 -0700

    address entity class complete with gets and sets

--- JPAProject/src/entities/Address.java
+++ JPAProject/src/entities/Address.java
@@ -8,15 +8,75 @@ import javax.persistence.Id;
 @Entity
 public class Address {
 	
+	
+	  //FIELDS
 	  @Id
 	    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	    private int id;
 	
-	
-	  private String city;
+	  	private String street;
+	  	private String street2;
+	  	private String city;
+	  	private String state;
+	  	private String zip;
+	  	private String country;
+	  	
+	  	
+	  	
+	  	//GETS AND SETS
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getStreet() {
+			return street;
+		}
+		public void setStreet(String street) {
+			this.street = street;
+		}
+		public String getStreet2() {
+			return street2;
+		}
+		public void setStreet2(String street2) {
+			this.street2 = street2;
+		}
+		public String getCity() {
+			return city;
+		}
+		public void setCity(String city) {
+			this.city = city;
+		}
+		public String getState() {
+			return state;
+		}
+		public void setState(String state) {
+			this.state = state;
+		}
+		public String getZip() {
+			return zip;
+		}
+		public void setZip(String zip) {
+			this.zip = zip;
+		}
+		public String getCountry() {
+			return country;
+		}
+		public void setCountry(String country) {
+			this.country = country;
+		}
+		
+		
+		//TOSTRING
+		@Override
+		public String toString() {
+			return "Address [id=" + id + ", street=" + street + ", street2=" + street2 + ", city=" + city + ", state="
+					+ state + ", zip=" + zip + ", country=" + country + "]";
+		}
+	  	
+	  	
+	  	
 	  
-	  private String address;
 	  
-	  private String country;
-
 }
