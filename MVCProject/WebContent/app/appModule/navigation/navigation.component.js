a62b05023762ccb4915316dde0127ba68a2a317c
alexisthelow <alexisthelow@gmail.com>
2018-01-12 15:54:01 -0700

    fixed token check error

--- MVCProject/WebContent/app/appModule/navigation/navigation.component.js
+++ MVCProject/WebContent/app/appModule/navigation/navigation.component.js
@@ -1,16 +1,17 @@
-angular.module('appModule')
-	.component('navigation', {
-		templateUrl : 'app/appModule/navigation/navigation.component.html',
-		controllerAs : 'vm',
-		controller : function(authService, $location){	
-			
-			var vm = this;
-	
+angular.module('appModule').component('navigation', {
+	templateUrl : 'app/appModule/navigation/navigation.component.html',
+	controller : function(authService, $location) {
+
+		var vm = this;
+
 		vm.loggedIn = function() {
-			if (authService.getToken()){
+			if (authService.getToken().id) {
+				console.log('returning true')
 				return true;
 			}
 			return false;
-			}
 		}
-	})
+	},
+
+	controllerAs : 'vm'
+})
